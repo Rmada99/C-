d69d89e626b2f97a78bb556d2caca8f5d42b42b2
TKD <ktc2241010004@edu.kyoto-tech.ac.jp>
2026-06-13 13:20:20 +0900

    課題１

---
+++ point_kadai1/point_kadai1/point_kadai1.cpp
@@ -0,0 +1,40 @@
+﻿
+#include <iostream>
+
+class IntPtr {
+public:
+	IntPtr(int* ptr) {
+		//
+		ptr_ = ptr;
+		std::cout << "生成" << std::endl ;
+	}
+
+	~IntPtr() {
+		//
+		delete ptr_;
+		std::cout << "破棄" << std::endl;
+	}
+
+	void set(int value) {
+		//
+		*ptr_ = value;
+	}
+
+	int get() const {
+		//
+		return *ptr_;
+	}
+
+private:
+	int* ptr_ = nullptr;
+};
+
+int main() {
+	IntPtr ptr(new int);
+
+	ptr.set(3);
+
+	int a = ptr.get();
+
+	std::make_unique<int>(5);
+}
