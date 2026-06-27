0a01b064bc90c9e24721c55645fcdb08f3fdde19
Rmada99 <ktc25a31a0057@edu.kyoto-tech.ac.jp>
2026-06-27 13:18:31 +0900

    課題１途中

---
+++ keisyou_kadai1/keisyou_kadai1/keisyou_kadai1.cpp
@@ -0,0 +1,35 @@
+﻿
+#include <iostream>
+
+class Animal{
+public:
+    Animal(){}
+    ~Animal(){}
+
+    virtual void cry() = 0;
+
+};
+
+class Dog :public Animal {
+    Dog(){}
+    ~Dog(){}
+
+    void cry()override {
+        std::cout << "わん" << std::endl;
+    }
+};
+
+class Cat :public Animal {
+    Cat() {}
+    ~Cat() {}
+
+    void cry()override {
+        std::cout << "にゃん" << std::endl;
+    }
+};
+
+
+int main()
+{
+    //Animal* animals[] = { new Animal,new Dog,new Cat };
+}
