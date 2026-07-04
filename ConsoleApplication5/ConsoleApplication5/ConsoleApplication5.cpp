aba719bd9a4ccf17c265c7e3434ec16720680391
Rmada99 <ktc25a31a0057@edu.kyoto-tech.ac.jp>
2026-07-04 12:15:51 +0900

    授業文

---
+++ ConsoleApplication5/ConsoleApplication5/ConsoleApplication5.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+#include<string>
+
+//
+template<class T>
+class Data {
+public:
+    void set(T data);
+    T get()const;
+
+private:
+    T value_ = 0;
+};
+
+template<class T>
+void Data<T>::set(T data) {
+    value_ = data;
+}
+
+template<class T>
+T Data<T>::get()const {
+    return value_;
+}
+
+//
+int main()
+{
+    Data<int> data;
+    data.set(100);
+    int d = data.get();
+
+    Data<float> data2;
+    data2.set(100.0f);
+    int d2 = data2.get();
+}
