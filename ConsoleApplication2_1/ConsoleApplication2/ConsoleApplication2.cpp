2bd5d85ee18e74d3334387d8543d189a1d943472
a <KTC25A31A0057@edu.kyoto-tech.ac.jp>
2026-08-22 15:53:00 +0900

    8/22

---
+++ ConsoleApplication2_1/ConsoleApplication2/ConsoleApplication2.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+
+// 再帰関数
+template <class T,class... Args>
+auto sumAll(T first, Args... args) {
+	if constexpr(sizeof...(args)!= 0) {
+		return first + sumAll(args...);
+	}
+	else {
+		return first;
+	}
+}
+
+int main() {
+	auto a1 = sumAll(1, 2.1, 3.2f, 4, 5);
+	auto a2 = sumAll(1, 2.05f, 3, 4, 5.99, 6, 7, 8.2, 9, 10);
+
+	std::cout << a1 << std::endl;
+	std::cout << a2 << std::endl;
+
+	return 0;
+}
